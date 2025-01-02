4ec17c59f5d4bc2df2d30fb05245c69fdcab2d2d
Dario Portmann <33952107+daerup@users.noreply.github.com>
2025-01-02 01:45:32 +0100

    feat: added interval env (#5)
    added interval env

--- ipinfo-loop.sh
+++ ipinfo-loop.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 while true; do
-    if wget -q --timeout=3 --spider http://google.com 2>/dev/null; then
-        ip_info=$(wget -q -O - http://ipinfo.io/ip)
+    ip_info=$(wget -q -O - http://ipinfo.io/ip 2>/dev/null)
+    
+    if [ -n "$ip_info" ]; then
         city=$(wget -q -O - http://ipinfo.io/city)
         country=$(wget -q -O - http://ipinfo.io/country)
         echo "IP: $ip_info - City: $city - Country: $country"
     else
-        echo "No internet connection detected. Is the Kill Switch enabled?"
+        echo "No internet connection detected or IP info service unavailable."
     fi
-    sleep 5
+    
+    sleep "${INTERVAL}"
 done
