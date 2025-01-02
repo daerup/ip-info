08e50b12692a28352c2bfbc5698c5f485f63eb37
Dario Portmann <daerup@gmx.ch>
2025-01-02 01:44:27 +0100

    added interval env

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,6 @@ COPY ./ipinfo-loop.sh /usr/local/bin/ipinfo-loop.sh
 
 RUN chmod +x /usr/local/bin/ipinfo-loop.sh
 
+ENV INTERVAL=5
+
 ENTRYPOINT ["/usr/local/bin/ipinfo-loop.sh"]
--- run.sh
+++
@@ -1,4 +0,0 @@
-# docker network create --internal --driver bridge internal-network
-# docker run -it --rm --network internal-network $(docker build -q .)
-
-docker run -it --rm $(docker build -q .)
